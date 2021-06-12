a6ece29c2ed2cc6b5a3caeb9b6a710a56474d96b
Sai Teja Pasula <saitejapasula@gmail.com>
2021-06-12 19:27:58 -0400

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -16,6 +16,10 @@ import logging
 import os
 from flask import Flask, render_template, request, Response
 import sqlalchemy
+from werkzeug.utils import secure_filename
+from google.cloud import storage
+import base64
+import logging
 
 global db
 
@@ -33,6 +37,21 @@ dir = '.ssl'
 if os.path.exists(dir):
     shutil.rmtree(dir)
 os.makedirs(dir)
+
+
+# setting gcs creds for access to bucket
+filecontents = os.environ.get('GCS_CREDS')
+filecontents = filecontents.replace("@", "=")
+decoded_creds = base64.b64decode(filecontents)
+with open('app/creds.json', 'wb') as f1:
+    f1.write(decoded_creds)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/app/creds.json'
+
+# getting bucket details
+bucket_name = os.environ.get('STORAGE_BUCKET')
+storage_client = storage.Client()
+bucket = storage_client.get_bucket('mgmt590-mayank')
+
 
 rootcertfile = os.environ.get('PG_SSLROOTCERT')
 rootcertfile = rootcertfile.replace('@', '=')
@@ -74,136 +93,6 @@ db_connect_string = " ".join([
     password,
     dbname
 ])
-# # storing all the information
-# database = 'mgmt590'
-# user = 'postgres'
-# password = 'prodscale'
-# # connecting to the database
-# connection = psycopg2.connect(database=database, user=user, password=password)
-# # instantiating the cursor
-# cursor = connection.cursor()
-#
-#
-#
-# def init_connection_engine():
-#     db_config = {
-#         # [START cloud_sql_postgres_sqlalchemy_limit]
-#         # Pool size is the maximum number of permanent connections to keep.
-#         "pool_size": 5,
-#         # Temporarily exceeds the set pool_size if no connections are available.
-#         "max_overflow": 2,
-#         # The total number of concurrent connections for your application will be
-#         # a total of pool_size and max_overflow.
-#         # [END cloud_sql_postgres_sqlalchemy_limit]
-#
-#         # [START cloud_sql_postgres_sqlalchemy_backoff]
-#         # SQLAlchemy automatically uses delays between failed connection attempts,
-#         # but provides no arguments for configuration.
-#         # [END cloud_sql_postgres_sqlalchemy_backoff]
-#
-#         # [START cloud_sql_postgres_sqlalchemy_timeout]
-#         # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-#         # new connection from the pool. After the specified amount of time, an
-#         # exception will be thrown.
-#         "pool_timeout": 30,  # 30 seconds
-#         # [END cloud_sql_postgres_sqlalchemy_timeout]
-#
-#         # [START cloud_sql_postgres_sqlalchemy_lifetime]
-#         # 'pool_recycle' is the maximum number of seconds a connection can persist.
-#         # Connections that live longer than the specified amount of time will be
-#         # reestablished
-#         "pool_recycle": 1800,  # 30 minutes
-#         # [END cloud_sql_postgres_sqlalchemy_lifetime]
-#     }
-#
-#     if os.environ.get("DB_HOST"):
-#         return init_tcp_connection_engine(db_config)
-#     else:
-#         return init_unix_connection_engine(db_config)
-#
-#
-# def init_tcp_connection_engine(db_config):
-#     # [START cloud_sql_postgres_sqlalchemy_create_tcp]
-#     # Remember - storing secrets in plaintext is potentially unsafe. Consider using
-#     # something like https://cloud.google.com/secret-manager/docs/overview to help keep
-#     # secrets secret.
-#     # db_user = os.environ["DB_USER"]
-#     # db_pass = os.environ["DB_PASS"]
-#     # db_name = os.environ["DB_NAME"]
-#     # db_host = os.environ["DB_HOST"]
-#
-#     db_user = os.environ.get('DB_USER')
-#     db_pass = os.environ.get('DB_PASS')
-#     db_name = os.environ.get('DB_NAME')
-#     db_host = os.environ.get('DB_HOST')
-#
-#
-#     # Extract host and port from db_host
-#     host_args = db_host.split(":")
-#     db_hostname, db_port = host_args[0], int(host_args[1])
-#
-#     pool = sqlalchemy.create_engine(
-#         # Equivalent URL:
-#         # postgresql+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
-#         sqlalchemy.engine.url.URL.create(
-#             drivername="postgresql+pg8000",
-#             username=db_user,  # e.g. "my-database-user"
-#             password=db_pass,  # e.g. "my-database-password"
-#             host=db_hostname,  # e.g. "127.0.0.1"
-#             port=db_port,  # e.g. 5432
-#             database=db_name  # e.g. "my-database-name"
-#         ),
-#         **db_config
-#     )
-#     # [END cloud_sql_postgres_sqlalchemy_create_tcp]
-#     pool.dialect.description_encoding = None
-#     return pool
-#
-#
-# def init_unix_connection_engine(db_config):
-#     # [START cloud_sql_postgres_sqlalchemy_create_socket]
-#     # Remember - storing secrets in plaintext is potentially unsafe. Consider using
-#     # something like https://cloud.google.com/secret-manager/docs/overview to help keep
-#     # secrets secret.
-#     # db_user = os.environ["DB_USER"]
-#     # db_pass = os.environ["DB_PASS"]
-#     # db_name = os.environ["DB_NAME"]
-#
-#     db_user = os.environ.get('DB_USER')
-#     db_pass = os.environ.get('DB_PASS')
-#     db_name = os.environ.get('DB_NAME')
-#     db_host = os.environ.get('DB_HOST')
-#
-#     db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-#     cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
-#
-#     pool = sqlalchemy.create_engine(
-#
-#         # Equivalent URL:
-#         # postgresql+pg8000://<db_user>:<db_pass>@/<db_name>
-#         #                         ?unix_sock=<socket_path>/<cloud_sql_instance_name>/.s.PGSQL.5432
-#         sqlalchemy.engine.url.URL.create(
-#             drivername="postgresql+pg8000",
-#             username=db_user,  # e.g. "my-database-user"
-#             password=db_pass,  # e.g. "my-database-password"
-#             database=db_name,  # e.g. "my-database-name"
-#             query={
-#                 "unix_sock": "{}/{}/.s.PGSQL.5432".format(
-#                     db_socket_dir,  # e.g. "/cloudsql"
-#                     cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-#             }
-#         ),
-#         **db_config
-#     )
-#     # [END cloud_sql_postgres_sqlalchemy_create_socket]
-#     pool.dialect.description_encoding = None
-#     return pool
-
-
-# This global variable is declared with a value of `None`, instead of calling
-# `init_connection_engine()` immediately, to simplify testing. In general, it
-# is safe to initialize your database connection pool when your script starts
-# -- there is no need to wait for the first request.
 
 
 def create_tables():
@@ -221,28 +110,6 @@ create_tables()
 
 app = Flask(__name__)
 
-# DATABASE_NAME = "prodscale.db"
-#
-#
-# def get_db():
-#     conn = sqlite3.connect(DATABASE_NAME)
-#     return conn
-
-
-# def create_tables():
-#     tables = [
-#         """CREATE TABLE IF NOT EXISTS prodscale(
-#                 timestamp INTEGER PRIMARY KEY,
-#                 model TEXT NOT NULL,
-#                 answer TEXT NOT NULL,
-#                 question TEXT NOT NULL,
-# 				context TEXT NOT NULL)
-#             """
-#     ]
-#     db = get_db()
-#     cursor = db.cursor()
-#     for table in tables:
-#         cursor.execute(table)
 
 
 modelList = [
@@ -279,26 +146,6 @@ def insert_db(timestamp, model, answer, question, context):
     cur.execute(postgres_insert_query, record_to_insert)
     conn.commit()
 
-    # stmt = sqlalchemy.text(
-    #     "INSERT INTO prodscale (timestamp, model, answer,question,context)"
-    #     " VALUES (:timestamp, :model, :answer,:question,:context)"
-    # )
-    # try:
-    #     # Using a with statement ensures that the connection is always released
-    #     # back into the pool at the end of statement (even if an error occurs)
-    #     with db.connect() as conn:
-    #         conn.execute(stmt, timestamp=timestamp, model=model,answer=answer,question=question,context=context)
-    # except Exception as e:
-    #     # If something goes wrong, handle the error in this section. This might
-    #     # involve retrying or adjusting parameters depending on the situation.
-    #     # [START_EXCLUDE]
-    #     logger.exception(e)
-    #     return Response(
-    #         status=500,
-    #         response="Unable to successfully insert values in databse! Please check the "
-    #                  "application logs for more details."
-    #     )
-
 
 def get_recent_default(start, end):
     conn = psycopg2.connect(db_connect_string)
@@ -309,7 +156,6 @@ def get_recent_default(start, end):
     cur.execute(postgres_insert_query, record_to_insert)
     result = cur.fetchall()
 
-    # result = conn.execute(stmt, start=start, end=end).fetchall()
     out = []
     for index, tuple in enumerate(result):
         dict = {
@@ -348,7 +194,20 @@ def get_recent_custom(start, end, model):
 def my_funct(text):
     abort(400, text)
 
+@app.route("upload", methods=['POST'])
+def upload_file():
+   if 'file' not in request.files:
+       return ('No file Provided')
+       file = request.files['file']
+       if file and allowed_file(file.filename):
+          dataFrame = pd.read_csv(file)
+          timestamp = int(time.time())
+          fileName = 'question_context' + '_' + str(timestamp) + '.csv'
+          csvFile = dataFrame.to_csv(fileName, index=False)
+          response = uploadOneFile(bucket, fileName)
+       return jsonify({"status": "File Uploaded Successfully", "status code": 200})
 
+    
 @app.route("/answer", methods=['POST', 'GET'])
 def answers():
     if request.method == 'POST':
@@ -493,20 +352,5 @@ def getModels(modelList=modelList):
 
 # Run if running "python answer.py"
 if __name__ == '__main__':
-    # Run our Flask app and start listening for requests!
-
-    # with db.connect() as conn:
-    #     conn.execute(
-    #         "CREATE TABLE IF NOT EXISTS prodscale(timestamp INT PRIMARY KEY,model TEXT NOT NULL,answer TEXT NOT NULL,question TEXT NOT NULL,context TEXT NOT NULL);")
-
-    # os.environ["DB_USER"] = "postgres"
-    # os.environ["DB_NAME"] = "postgres-prodscale"
-    # os.environ["DB_PASS"] = "prodscale"
-    # os.environ["DB_HOST"] = "35.232.200.40:5432"
     default_model = modelList[0]
-    
-    # with db.connect() as conn:
-    #     conn.execute(
-    #         "CREATE TABLE IF NOT EXISTS prodscale(timestamp INT PRIMARY KEY,model TEXT NOT NULL,answer TEXT NOT NULL,question TEXT NOT NULL,context TEXT NOT NULL);")
-
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), threaded=True)
